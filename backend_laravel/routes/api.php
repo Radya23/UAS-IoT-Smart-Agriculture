e54aef948664e2543d9a81a30912be6dc723ce1d
Arysrzb <Arysrizki18@gmail.com>
2026-06-24 16:59:01 +0700

    Delete backend_laravel directory

--- backend_laravel/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SensorController;
-
-Route::post('/sensor', [SensorController::class, 'store']);
-Route::get('/sensor/latest', [SensorController::class, 'latest']);
-Route::post('/sensor/update-status', [SensorController::class, 'updateStatus']);
-Route::get('/sensor/status', [SensorController::class, 'getStatus']);

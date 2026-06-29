9484123717b27aaa6b12658b0a51552714110918
Radya <radyadivena994@gmail.com>
2026-06-29 20:10:14 +0700

    Update api.php

--- backend_laravel/routes/api.php
+++ backend_laravel/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SensorController;
 
-Route::post('/sensor', [SensorController::class, 'store']);
-Route::get('/sensor/latest', [SensorController::class, 'latest']);
-Route::post('/sensor/update-status', [SensorController::class, 'updateStatus']);
+Route::post('/sensor/update-otomatis', [SensorController::class, 'updateSensor']);
 Route::get('/sensor/status', [SensorController::class, 'getStatus']);
+Route::get('/sensor/histori', [SensorController::class, 'getHistori']);
